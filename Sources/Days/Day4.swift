8811ff80ace45c17ca870c58a773aa4bfd3425ee
Rik van Toor <rik.van.toor@gmail.com>
2024-12-05 09:18:53 +0100

    Slightly simplified day 4

--- Sources/Days/Day4.swift
+++ Sources/Days/Day4.swift
@@ -7,20 +7,17 @@ class Day4: Day {
   }
 
   func part1(_ input: Input) -> Part1 {
-    let goal = Array("XMAS")
+    let goal: [Character] = Array("XMAS")
     return (0...input.count-1).map({y in 
       (0...input[y].count-1).map({x in 
-        let l = input[y]
-        let dxs = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
-        return dxs.map({ (dx, dy) in
-          if (0...3).allSatisfy({ p in
+        let l: [Character] = input[y]
+        let dxs: [(Int, Int)] = [(-1, 0), (-1, -1), (0, -1), (1, -1), (1, 0), (1, 1), (0, 1), (-1, 1)]
+        return dxs.filter({ (dx, dy) in
+          return (0...3).allSatisfy({ p in
             let (x2, y2) = (x + p * dx, y + p * dy)
             return y2 >= 0 && y2 < l.count && x2 >= 0 && x2 < l.count && input[y2][x2] == goal[p]
-          }) {
-            return 1
-          }
-          return 0
-        }).reduce(0, +)
+          })
+        }).count
       }).reduce(0, +)
     }).reduce(0, +)
   }
