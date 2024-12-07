e9df87c5db8930a6b5bf83fcbc4cbd323571c536
Rik van Toor <rik.van.toor@gmail.com>
2024-12-07 10:52:28 +0100

    Concurrency for day 7

--- Sources/Days/Day7.swift
+++ Sources/Days/Day7.swift
@@ -28,37 +28,61 @@ class Day7: Day {
     }
   }
 
-  func solve(goal: Int, current: Int, steps: ArraySlice<Int>, ops: [(Int, Int) -> Int]) -> Bool {
+  func run(input: Input, ops: Ops) async -> Int {
+    let tasks = input.map {(goal, ns) in
+      Task {
+        if solve(goal: goal, current: ns[0], steps: ns[1...], ops: ops) {
+          goal
+        } else {
+          0
+        }
+      }
+    }
+    var sum = 0
+    for t in tasks {
+      sum += await t.result.get()
+    }
+    return sum
+  }
+
+  func part1(_ input: Input) async -> Part1 {
+    await run(input: input, ops: .part1)
+  }
+
+  func part2(_ input: Input) async -> Part2 {
+    await run(input: input, ops: .part2)
+  }
+}
+
+enum Ops {
+  case part1
+  case part2
+}
+
+nonisolated(unsafe) let ops1: [(Int,Int) -> Int] = [(+), (*)]
+nonisolated(unsafe) let ops2: [(Int,Int) -> Int] = [(+), (*), {(x,y) in x * pow(10, String(y).count) + y}]
+func getOps(_ o: Ops) -> [(Int,Int) -> Int] {
+  switch o {
+    case .part1:
+      ops1
+    case .part2:
+      ops2
+  }
+}
+
+func solve(goal: Int, current: Int, steps: ArraySlice<Int>, ops: Ops) -> Bool {
     if steps.count == 0 {
       return current == goal
     }
     let next = steps.first!
     let tail = steps.dropFirst()
-    return ops.map {o in 
+    return getOps(ops).map {o in 
       solve(goal: goal, current: o(current, next), steps: tail, ops: ops)
     }.reduce(false, {(a,b) in 
       a || b
     })
   }
 
-  func run(input: Input, ops: [(Int, Int) -> Int]) -> Int {
-    input.map {(goal, ns) in
-      if solve(goal: goal, current: ns[0], steps: ns[1...], ops: ops) {
-        goal
-      } else {
-        0
-      }
-    }.reduce(0, +)
-  }
-
-  func part1(_ input: Input) -> Part1 {
-    run(input: input, ops: [(+), (*)])
-  }
-
-  func part2(_ input: Input) -> Part2 {
-    run(input: input, ops: [(+), (*), {(x,y) in x * pow(10, String(y).count) + y}])
-  }
-}
 
 func pow(_ x: Int, _ y: Int) -> Int {
     if y == 1 {
